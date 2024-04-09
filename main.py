8facbeea69cecee1a41240d7d072443eae5c82b1
FantasySilence <1735793009@qq.com>
2024-04-10 02:00:11 +0800

    添加了TODO list

--- main.py
+++ main.py
@@ -10,3 +10,11 @@ import sys
 # 配置import路径信息
 ROOTPATH = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(ROOTPATH)
+
+"""
+TODO:
+1. 复刻论文，实现论文中相关的算法
+2. 预处理与特征工程的模块
+3. 组织主程序入口
+4. 实现一个模块用于操作const.py中内容的增删查改
+"""
