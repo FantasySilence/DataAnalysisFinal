27cebe4c3e71b8b01dc7615fdac56fef700f2534
FantasySilence <1735793009@qq.com>
2024-04-14 22:57:35 +0800

    修改主程序入口

--- main.py
+++ main.py
@@ -4,12 +4,6 @@
 # @IDE: Visual Studio Code & PyCharm  #
 # @Python: 3.9.7                      #
 # =================================== #
-import os
-import sys
-
-# 配置import路径信息
-ROOTPATH = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(ROOTPATH)
 
 """
 TODO:
