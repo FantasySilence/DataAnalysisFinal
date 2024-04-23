60ca28e3078607da661b6ce89d5a6a4b669e404f
FantasySilence <1735793009@qq.com>
2024-04-23 16:04:07 +0800

    修改了评估模型绘图的对象基类

--- src/common/figTool/evaluateobjectbase.py
+++ src/common/figTool/evaluateobjectbase.py
@@ -7,6 +7,7 @@
 # @Description: Object base class for Various model evaluation indicators    #
 # ========================================================================== #
 import os
+import matplotlib.pyplot as plt
 
 from src.common.fileTool.figuresio import FiguresIO
 
@@ -17,7 +18,10 @@ class EvaluateObjectBase:
     这是一个对象基类，用于评估模型时进行参数初始化
     """
 
-    def __init__(self) -> None:
+    def __init__(
+            self, ax: plt.Axes=None, fig_name: str=None,
+            is_show_alone: bool=True, is_show: bool=True, is_save: bool=False,
+    ) -> None:
         
         # ------ 创建存放图片的文件夹 ------ #
         self.folder_name = "models_figures"
@@ -28,4 +32,11 @@ class EvaluateObjectBase:
             os.mkdir(data_folder)
         else:
             pass
-    
+
+        # ------ 绘图参数 ------ #
+        self.is_show_alone = is_show_alone
+        self.is_show = is_show
+        self.is_save = is_save
+        self.ax = ax
+        self.fig_name = fig_name
+        
