baf67f2dad942c4e220c4285bf87b63dc9ed4674
FantasySilence <1735793009@qq.com>
2024-04-24 22:38:55 +0800

    重新组织主程序入口

---
+++ main_mymodule.py
@@ -0,0 +1,8 @@
+# =================================== #
+# @Author: Fantasy_Silence            #
+# @Time: 2024-04-24                   #
+# @IDE: Visual Studio Code & PyCharm  #
+# @Python: 3.9.7                      #
+# =================================== #
+# @Description: Using my data         #
+# =================================== #
