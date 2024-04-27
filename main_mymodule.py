d841c5dc32dd82047a580808f2a65bcfd415eab6
FantasySilence <1735793009@qq.com>
2024-04-27 17:51:13 +0800

    更新了TODO list

--- main_mymodule.py
+++ main_mymodule.py
@@ -100,3 +100,29 @@ print(
 # ================
 # 3.模型训练
 # ================
+# TODO: 可能会使用到的模型List:
+"""
+我计划直接根据近期的房产数据预测某个位置的房价
+但实际中这可能较难做到。不过消费者可以很清楚的知道该市房价的平均水平
+因此可以将房价划分为两类，进行分类任务，预测某个位置的房价是否高于平均水平
+一、计划使用的回归模型
+1. XGBoost Regressor
+2. AdaBoost Regressor
+3. Gradient Boosting Regressor
+4. Random Forest Regressor
+5. Decision Tree Regressor
+6. SVM Regressor
+7. SGD Regressor
+8. Linear Regressor
+9. Elastic Net Regressor
+二、计划使用的分类模型(二分类)
+1. XGBClassifier
+2. AdaBoostClassifier
+3. GradientBoostingClassifier
+4. RandomForestClassifier
+5. DecisionTreeClassifier
+6. SVC
+7. SGDClassifier
+8. LinearDiscriminantAnalysis
+9. LogisticRegression
+"""
