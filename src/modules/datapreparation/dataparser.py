29f272d42ce28e9d5d3551eca0b3ed4e97197f67
FantasySilence <1735793009@qq.com>
2024-04-29 03:15:58 +0800

    完成了解析器的重构

--- src/modules/datapreparation/dataparser.py
+++ src/modules/datapreparation/dataparser.py
@@ -11,10 +11,10 @@ import os
 import numpy as np
 import pandas as pd
 from lxml import etree
-from functools import reduce
 
 from src.common.fileTool.filesio import FilesIO
 from src.common.infoTool.const import CONST_TABLE
+from src.common.locTool.poiinfo import POICollector
 from src.common.locTool.lnglat import GetLongitudeLatitude
 
 
@@ -41,20 +41,35 @@ class HousingDataParser:
             self.city = city
 
         # ------ 初始化存储爬取信息的列表，便于存入csv ------ #
-        self.house_price_list = []          # 存储房价
-        self.unit_price_list = []           # 存储每平方米价格
-        self.house_area_list = []           # 存储房子面积
-        self.house_loc_list = []            # 存储房子地址
-        self.house_bedroom_list = []        # 存储房子卧室数量
-        self.house_room_list = []           # 存储房子房间数量
-        self.house_orientation_list = []    # 存储房子朝向
-        self.house_age_list = []            # 存储房子年龄
-        self.longitude_list = []            # 存储房子经度
-        self.latitude_list = []             # 存储房子维度
+        self.house_price_list = []              # 存储房价
+        self.unit_price_list = []               # 存储每平方米价格
+        self.house_area_list = []               # 存储房子面积
+        self.house_loc_list = []                # 存储房子地址
+        self.house_bedroom_list = []            # 存储房子卧室数量(室)
+        self.house_livingroom_list = []         # 存储房间客厅数量(厅)
+        self.house_bathroom_list = []           # 存储房子卫生间数量(卫)
+        self.house_orientation_list = []        # 存储房子朝向
+        self.house_age_list = []                # 存储房子年龄
+        self.longitude_list = []                # 存储房子经度
+        self.latitude_list = []                 # 存储房子纬度
+        self.house_subway_list = []             # 存储房子是否临近地铁
+        self.house_floor_type_list = []         # 存储房子所在楼层高低
+        self.house_floor_sum_list = []          # 存储房子所在楼层总数
+        self.house_housing_period_list = []     # 存储房子房本年限
+        self.school_around_list = []            # 存储房子周围学校数量
+        self.subway_around_list = []            # 存储房子周围地铁站数量
+        self.park_around_list = []              # 存储房子周围公园数量
+        self.shopping_mall_around_list = []     # 存储房子周围购物商场数量
+        self.bus_around_list = []               # 存储房子周围购物商城数量
 
         # ------ 提取 ------ #
         self._parse_data()
         self._parse_loc_to_lnglat()
+        self._parse_bus_around()
+        self._parse_school_around()
+        self._parse_park_around()
+        self._parse_shopping_mall_around()
+        self._parse_subway_around()
 
         # ------ 创建存放数据的文件夹 ------ #
         folder_name = "row_data"
@@ -120,20 +135,20 @@ class HousingDataParser:
                 house_loc[i] += community_loc[i]
             self.house_loc_list.extend(house_loc)
 
-            # ------ 解析房屋房间数量信息，并存储 ------ #
-            house_room = tree.xpath(CONST_TABLE["XPATH"]["HOUSE_ROOM_NUM"])
-            i, house_room_patten_list = 5, []
-            while i <= len(house_room):
-                room_patten = "".join(house_room[i-5: i+1])
-                i += 6
-                room_num = sum(int(i) for i in room_patten if i.isdigit())
-                house_room_patten_list.append(room_num)
-            self.house_room_list.extend(house_room_patten_list)
-
             # ------ 解析房屋卧室数量信息，并存储 ------ #
             house_bedroom = tree.xpath(CONST_TABLE["XPATH"]["HOUSE_BEDROOM_NUM"])
             house_bedroom_list = [i.strip() for i in house_bedroom]
             self.house_bedroom_list.extend(house_bedroom_list)
+
+            # ------ 解析房屋客厅数量信息，并存储 ------ #
+            house_livingroom = tree.xpath(CONST_TABLE["XPATH"]["HOUSE_LIVINGROOM_NUM"])
+            house_livingroom_list = [i.strip() for i in house_livingroom]
+            self.house_livingroom_list.extend(house_livingroom_list)
+
+            # ------ 解析房屋卫生间数量信息，并存储 ------ #
+            house_toilet = tree.xpath(CONST_TABLE["XPATH"]["HOUSE_BATHROOM_NUM"])
+            house_toilet_list = [i.strip() for i in house_toilet]
+            self.house_bathroom_list.extend(house_toilet_list)
 
             # ------ 解析房屋朝向信息，并存储 ------ #
             house_orientation = tree.xpath(CONST_TABLE["XPATH"]["HOUSE_FACING"])
@@ -148,101 +163,9 @@ class HousingDataParser:
             ]
             self.house_age_list.extend(house_age_list)
 
-            # ------ 解析进度 ------ #
-            print(
-                "Parsing...", 
-                f'|{"■" * ((j + 1) * 50 // 51):50}|', 
-                f'{(j + 1) * 100 // 51}%', end='\r'
-            )
-
-        # ------ 临时存入DataFrame, 用于去重 ------ #
-        self.df = pd.DataFrame({
-            "houseLoc": self.house_loc_list, "unitPrice": self.unit_price_list, 
-            "housePrice": self.house_price_list, "houseArea": self.house_area_list,  
-            "houseBedroom": self.house_bedroom_list, "houseRoom": self.house_room_list, 
-            "houseOrientation": self.house_orientation_list, "houseAge": self.house_age_list, 
-        })
-        self.df.drop_duplicates(inplace=True, subset=["houseLoc"], keep="first")
-        print("\nParsing complete...")
-            
-    
-    def _parse_loc_to_lnglat(self):
-
-        """
-        将房屋的地址信息转换为经纬度
-        """
-
-        print("Parsing location to longitude and latitude...")
-        # ------ 转换 ------ #
-        for i in range(len(self.df["houseLoc"].tolist())):
-            lnglat = GetLongitudeLatitude(
-                city=CONST_TABLE["CITY"][self.city], 
-                address=self.df["houseLoc"].tolist()[i]
-            )
-            self.longitude_list.append(lnglat.longtitude)
-            self.latitude_list.append(lnglat.latitude)
-            
-            # ------ 解析进度 ------ #
-            print(
-                "Parsing...", 
-                f'|{"■" * ((i + 1) * 50 // len(self.df["houseLoc"].tolist())):50}|', 
-                f'{(i + 1) * 100 // len(self.df["houseLoc"].tolist())}%', end='\r'
-            )
-        
-        # ------ 生成最终的DataFrame ------ #
-        self.df.insert(1, "longitude", self.longitude_list)
-        self.df.insert(2, "latitude", self.latitude_list)
-        self.df.insert(0, "ID", range(1, len(self.df) + 1))
-        print("\nParsing complete...")
-    
-
-    @staticmethod
-    def parse_more(city: str) -> None:
-
-        """
-        用于解析更多信息的接口
-        """
-
-        # ------ 重新读取之前解析好的信息 ------ #
-        data = pd.read_csv(FilesIO.getDataset(
-            "row_data/%s_housing_data.csv" % city
-        ))
-        
-        # ------ 初始化存储爬取信息的列表，便于存入csv ------ #
-        key_house_loc_list = []         # 作为键值用于合并
-        house_subway_list = []          # 存储房子是否临近地铁
-        house_floor_type_list = []      # 存储房子所在楼层高低
-        house_floor_sum_list = []       # 存储房子所在楼层总数
-        house_housing_period_list = []  # 存储房子房本年限
-
-        print("Parsing more data...")
-        for j in range(1, 51):
-
-            # ------ 重新读取html文件 ------ #
-            path = FilesIO.getHTMLtext(
-                "%s_htmls/%s_page_%d.html" % (city, city, j)
-            )
-
-            # ------ 解析存储信息的div标签 ------ #
-            parser = etree.HTMLParser(encoding="utf-8")
-            tree = etree.parse(path, parser=parser)
-            divs = tree.xpath(CONST_TABLE["XPATH"]["ROOT_PATH"])
-
-            # ------ 解析房屋地址信息，并作为键值存储 ------ #
-            community_loc = tree.xpath(CONST_TABLE["XPATH"]["COMMUNITY_LOCATION"])
-            house_loc = tree.xpath(CONST_TABLE["XPATH"]["HOUSE_LOCATION"])
-            house_loc_list = [i.strip() for i in house_loc]
-            i = 2
-            house_loc = []
-            while i < len(house_loc_list):
-                house_loc.append(house_loc_list[i-2] + house_loc_list[i-1] + house_loc_list[i])
-                i += 3
-            for i in range(len(community_loc)):
-                house_loc[i] += community_loc[i]
-            key_house_loc_list.extend(house_loc)
-
             # ------ 信息存储在房屋信息的tag中，提取这些tag ------ #
             tag_info = []
+            divs = tree.xpath(CONST_TABLE["XPATH"]["ROOT_PATH"])
             for div in divs:
                 spans = div.xpath(CONST_TABLE["XPATH"]["HOUSE_TAG"])
                 near_metro = []
@@ -255,7 +178,7 @@ class HousingDataParser:
                 1 if "近地铁" in item else 0 
                 for item in tag_info
             ]
-            house_subway_list.extend(is_near_metro)
+            self.house_subway_list.extend(is_near_metro)
 
             # ------ 解析房屋的房本年限 ------ #
             fangben_info = []
@@ -266,7 +189,7 @@ class HousingDataParser:
                     fangben_info.append("满二年")
                 else:
                     fangben_info.append(np.nan)
-            house_housing_period_list.extend(fangben_info)
+            self.house_housing_period_list.extend(fangben_info)
 
             # ------ 解析房屋的楼层(高中低)与楼层总数 ------ #
             info_list = []      # 获取存储信息的div标签中的所有文字
@@ -281,43 +204,221 @@ class HousingDataParser:
                 try:
                     # 获取楼层高低以及总层数
                     if item[2][:2] in ["低层", "中层", "高层"]:
-                        house_floor_type_list.append(item[2][:2])
-                        house_floor_sum_list.append(
+                        self.house_floor_type_list.append(item[2][:2])
+                        self.house_floor_sum_list.append(
                             int("".join([i for i in item[2] if i.isdigit()]))
                         )
                     else:
                         # 楼层高低可能缺失
-                        house_floor_type_list.append(np.nan)
-                        house_floor_sum_list.append(
+                        self.house_floor_type_list.append(np.nan)
+                        self.house_floor_sum_list.append(
                             int("".join([i for i in item[2] if i.isdigit()]))
                         )
                 # 楼层高低可能缺失，总层数也可能缺失
                 except IndexError:
-                    house_floor_type_list.append(np.nan)
-                    house_floor_sum_list.append(np.nan)
-            
+                    self.house_floor_type_list.append(np.nan)
+                    self.house_floor_sum_list.append(np.nan)
+
             # ------ 解析进度 ------ #
             print(
                 "Parsing...", 
                 f'|{"■" * ((j + 1) * 50 // 51):50}|', 
                 f'{(j + 1) * 100 // 51}%', end='\r'
             )
-        
-        # ------ 与data合并重新储存 ------ #
-        # 临时创建DataFrame存储
-        df = pd.DataFrame({
-            "houseLoc": key_house_loc_list, "houseSubway": house_subway_list,
-            "houseHousingPeriod": house_housing_period_list,
-            "houseFloorType": house_floor_type_list, 
-            "houseFloorSum": house_floor_sum_list
+
+        # ------ 临时存入DataFrame, 用于去重 ------ #
+        self.df = pd.DataFrame({
+            "houseLoc": self.house_loc_list, 
+            "unitPrice": self.unit_price_list, 
+            "housePrice": self.house_price_list, 
+            "houseArea": self.house_area_list,  
+            "houseBedroom": self.house_bedroom_list, 
+            "houseLivingRoom": self.house_livingroom_list,
+            "houseBathroom": self.house_bathroom_list, 
+            "houseOrientation": self.house_orientation_list, 
+            "houseAge": self.house_age_list, 
+            "houseSubway": self.house_subway_list, 
+            "houseHousingPeriod": self.house_housing_period_list,
+            "houseFloorType": self.house_floor_type_list,
+            "houseFloorSum": self.house_floor_sum_list,
         })
-        df.drop_duplicates(inplace=True, subset=["houseLoc"], keep="first")
+        self.df.drop_duplicates(inplace=True, subset=["houseLoc"], keep="first")
+        print("\nParsing complete...")
+            
+    
+    def _parse_loc_to_lnglat(self) -> None:
+
+        """
+        将房屋的地址信息转换为经纬度
+        """
+
+        print("Parsing location to longitude and latitude...")
+        # ------ 转换 ------ #
+        for i in range(len(self.df["houseLoc"].tolist())):
+            true_lnglat = GetLongitudeLatitude(
+                city=CONST_TABLE["CITY"][self.city], 
+                address=self.df["houseLoc"].tolist()[i]
+            )
+            self.longitude_list.append(true_lnglat.longtitude)
+            self.latitude_list.append(true_lnglat.latitude)
+            
+            # ------ 解析进度 ------ #
+            print(
+                "Parsing...", 
+                f'|{"■" * ((i + 1) * 50 // len(self.df["houseLoc"].tolist())):50}|', 
+                f'{(i + 1) * 100 // len(self.df["houseLoc"].tolist())}%', end='\r'
+            )
         
-        # 以houseLoc为键值进行合并
-        res = pd.merge(data, df, on="houseLoc")
-        res.to_csv(
-            FilesIO.getDataset("%s_housing_data.csv" % city), 
-            index=False, encoding="utf-8-sig"
+        # ------ 添加到最终的DataFrame ------ #
+        self.df.insert(1, "longitude", self.longitude_list)
+        self.df.insert(2, "latitude", self.latitude_list)
+        self.df.insert(0, "ID", range(1, len(self.df) + 1))
+        self.df.reset_index(drop=True, inplace=True)
+        print("\nParsing complete...")
+
+
+    def _parse_school_around(self) -> None:
+
+        """
+        解析周边学校信息
+        """
+
+        print("Parsing school around...")
+        for i in range(len(self.df["houseLoc"].tolist())):
+            self.school_around_list.append(
+                POICollector(
+                    query="学校", lat=self.df.loc[i, "latitude"], 
+                    lng=self.df.loc[i, "longitude"], radius=1000
+                ).num_of_query
+            )
+            
+            # ------ 解析进度 ------ #
+            print(
+                "Parsing...", 
+                f'|{"■" * ((i + 1) * 50 // len(self.df["houseLoc"].tolist())):50}|', 
+                f'{(i + 1) * 100 // len(self.df["houseLoc"].tolist())}%', end='\r'
+            )
+        
+        # ------ 添加到最终的DataFrame ------ #
+        self.df.insert(
+            len(self.df.columns), "schoolAround", self.school_around_list
         )
         print("\nParsing complete...")
+    
+
+    def _parse_subway_around(self) -> None:
+
+        """
+        解析周边地铁信息
+        """
+
+        print("Parsing subway around...")
+        for i in range(len(self.df["houseLoc"].tolist())):
+            self.subway_around_list.append(
+                POICollector(
+                    query="地铁", lat=self.df.loc[i, "latitude"], 
+                    lng=self.df.loc[i, "longitude"], radius=1000
+                ).num_of_query
+            )
+
+            # ------ 解析进度 ------ #
+            print(
+                "Parsing...", 
+                f'|{"■" * ((i + 1) * 50 // len(self.df["houseLoc"].tolist())):50}|', 
+                f'{(i + 1) * 100 // len(self.df["houseLoc"].tolist())}%', end='\r'
+            )
         
+        # ------ 添加到最终的DataFrame ------ #
+        self.df.insert(
+            len(self.df.columns), "subwayAround", self.subway_around_list
+        )
+        print("\nParsing complete...")
+
+
+    def _parse_park_around(self) -> None:
+
+        """
+        解析周边公园信息
+        """
+
+        print("Parsing park around...")
+        for i in range(len(self.df["houseLoc"].tolist())):
+            self.park_around_list.append(
+                POICollector(
+                    query="公园", lat=self.df.loc[i, "latitude"], 
+                    lng=self.df.loc[i, "longitude"], radius=1000
+                ).num_of_query
+            )
+
+            # ------ 解析进度 ------ #
+            print(
+                "Parsing...", 
+                f'|{"■" * ((i + 1) * 50 // len(self.df["houseLoc"].tolist())):50}|', 
+                f'{(i + 1) * 100 // len(self.df["houseLoc"].tolist())}%', end='\r'
+            )
+        
+        # ------ 添加到最终的DataFrame ------ #
+        self.df.insert(
+            len(self.df.columns), "parkAround", self.park_around_list
+        )
+        print("\nParsing complete...")
+    
+
+    def _parse_shopping_mall_around(self) -> None:
+
+        """
+        解析周边购物商城信息
+        """
+
+        print("Parsing shopping mall around...")
+        for i in range(len(self.df["houseLoc"].tolist())):
+            self.shopping_mall_around_list.append(
+                POICollector(
+                    query="购物", lat=self.df.loc[i, "latitude"], 
+                    lng=self.df.loc[i, "longitude"], radius=1000
+                ).num_of_query
+            )
+
+            # ------ 解析进度 ------ #
+            print(
+                "Parsing...", 
+                f'|{"■" * ((i + 1) * 50 // len(self.df["houseLoc"].tolist())):50}|', 
+                f'{(i + 1) * 100 // len(self.df["houseLoc"].tolist())}%', end='\r'
+            )
+        
+        # # ------ 添加到最终的DataFrame ------ #
+        self.df.insert(
+            len(self.df.columns), "shopping_mallAround", 
+            self.shopping_mall_around_list
+        )
+        print("\nParsing complete...")
+    
+    
+    def _parse_bus_around(self) -> None:
+
+        """
+        解析周边公交站台信息
+        """
+
+        print("Parsing bus around...")
+        for i in range(len(self.df["houseLoc"].tolist())):
+            self.bus_around_list.append(
+                POICollector(
+                    query="公交", lat=self.df.loc[i, "latitude"], 
+                    lng=self.df.loc[i, "longitude"], radius=1000
+                ).num_of_query
+            )
+
+            # ------ 解析进度 ------ #
+            print(
+                "Parsing...", 
+                f'|{"■" * ((i + 1) * 50 // len(self.df["houseLoc"].tolist())):50}|', 
+                f'{(i + 1) * 100 // len(self.df["houseLoc"].tolist())}%', end='\r'
+            )
+        
+        # ------ 添加到最终的DataFrame ------ #
+        self.df.insert(
+            len(self.df.columns), "busAround", 
+            self.bus_around_list
+        )
+        print("\nParsing complete...")
